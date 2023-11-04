34ce2a1148befa4b2f4a4ade20f511d26d4a5969
Hydrostic <hydrostic@outlook.com>
2023-11-04 15:08:13 +0800

    feature(core):初次提交

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
+use salvo::prelude::*;
+
+#[handler]
+async fn hello() -> &'static str {
+    "Hello World"
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::filter::LevelFilter::INFO)
+        .with(fmt::layer())
+        .init();
+    tracing::info!("Rustle Blog v1.0.0({})", env!("GIT_HASH"));
+    let router = Router::new().get(hello);
+    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
+    Server::new(acceptor).serve(router).await;
+}
